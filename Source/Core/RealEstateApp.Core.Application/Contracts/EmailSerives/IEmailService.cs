7b81b37ed6a2e798ec271df14895d0a38b311563
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-07-07 12:00:41 -0400

    Incorporación de servicio de correo, DTOs y actualización de perfiles de ejecución
    - Se añadió la interfaz `IEmailService` para el envío de correos electrónicos.
    - Se introdujo `MessageDto` para representar la estructura del mensaje de correo.
    - Se agregó la clase `EmailSettings` para los detalles de configuración del correo.
    - Se actualizó `RealEstateApp.Core.Application.csproj` con una nueva estructura de carpetas para los contratos de usuario.
    - Se habilitó `UserSecretsId` en `RealEstateApp.Presentation.Api.csproj` para el almacenamiento seguro de configuraciones.
    - Se actualizaron los ajustes de ejecución de la solución para incluir un nuevo perfil destinado a iniciar los proyectos de API y WebApp.

---
+++ Source/Core/RealEstateApp.Core.Application/Contracts/EmailSerives/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using RealEstateApp.Core.Application.DTOs.Message;
+
+namespace RealEstateApp.Core.Application.Contracts.EmailSerives
+{
+    public  interface IEmailService
+    {
+        Task<bool> SendEmailAsync(MessageDto message);
+    }
+}
