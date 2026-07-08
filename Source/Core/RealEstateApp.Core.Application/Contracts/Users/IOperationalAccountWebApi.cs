f0a1331593c46d30076799f50f35dfa345fa701b
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-07-08 19:53:22 -0400

    Refactor and expand user account interfaces
    Updated `IBaseAccountUser` with an empty interface. Changed access modifiers of `IOperationalAccountWebApi` and `IOperationalAccountWebApp` from `internal` to `public`. Added necessary `using` directives and new methods to `IOperationalAccountWebApp` for handling user-related operations, including fetching clients, agents, and password management.

--- Source/Core/RealEstateApp.Core.Application/Contracts/Users/IOperationalAccountWebApi.cs
+++ Source/Core/RealEstateApp.Core.Application/Contracts/Users/IOperationalAccountWebApi.cs
@@ -1,6 +1,7 @@
 ﻿namespace RealEstateApp.Core.Application.Contracts.Users
 {
-    internal interface IOperationalAccountWebApi
+    public interface IOperationalAccountWebApi
     {
+
     }
 }
--- Source/Core/RealEstateApp.Core.Application/Contracts/Users/IOperationalAccountWebApp.cs
+++ Source/Core/RealEstateApp.Core.Application/Contracts/Users/IOperationalAccountWebApp.cs
@@ -1,6 +1,17 @@
-﻿namespace RealEstateApp.Core.Application.Contracts.Users
+﻿using RealEstateApp.Core.Application.DTOs.Users.DtoQueryUser;
+using RealEstateApp.Core.Application.DTOs.Users.Password;
+using RealEstateApp.Core.Application.DTOs.Users.Response;
+
+namespace RealEstateApp.Core.Application.Contracts.Users
 {
-    internal interface IOperationalAccountWebApp
+    public interface IOperationalAccountWebApp
     {
+        Task<IReadOnlyCollection<ClientDto>> GetClientAllAsync(List<string> Ids);
+        Task<IReadOnlyCollection<ConsultAgentDto>> GetAgentAllAsync(List<string> Ids);
+        Task<IReadOnlyCollection<CustomerConsultAgentDto>> GetAgentByConsultCustomerAsync();
+        Task<IReadOnlyCollection<GetInternalUserDto>> GetInternalUserGetAll();
+        Task<IReadOnlyCollection<AdminConsultAgentDto>> GetAgentByConsultAdminAll();
+        Task<UserResponseDto> ForgoutPasswordAsync(ForgoutPasswordDto forgoutPasswordDto);
+        Task<UserResponseDto> ResetPasswordAsync(ResetPasswordDto resetPasswordDto);
     }
 }
