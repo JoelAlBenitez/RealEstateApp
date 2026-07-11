b0af1f161d3a88256e413089687af64d02f7bf7b
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-07-11 13:34:54 -0400

    Implement authentication and email token features
    Enhanced `AuthServicesPrcocessAccountWebApp` with core authentication functionalities, including email confirmation, password reset, login validation, and activation email resending. Introduced `EmailConfirmProviderTokensConfigurations` and `EmailConfirmProviderOptions` for token generation and management. Cleaned up unused folder references in the project file.

---
+++ Source/Infraestructure/RealEstateApp.Infraestructure.Identity/EmailProvider/EmailConfirmProviderTokensConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RealEstateApp.Infraestructure.Identity.Entities;
+using RealEstateApp.Infrastructure.Identity.EmailProvider.Options;
+
+namespace RealEstateApp.Infrastructure.Identity.EmailProvider
+{
+    public class EmailConfirmProviderTokensConfigurations :
+        DataProtectorTokenProvider<AppUsers>
+    {
+        public EmailConfirmProviderTokensConfigurations(
+            IDataProtectionProvider dataProtectionProvider, 
+            IOptions<EmailConfirmProviderOptions> options,
+            ILogger<DataProtectorTokenProvider<AppUsers>> logger) 
+            : base(dataProtectionProvider, options, logger)
+        {
+        }
+    }
+}
