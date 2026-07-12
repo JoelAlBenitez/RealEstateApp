25fd76dd23c4aa4ad7ac775e5111462ca9612099
Adrixn23 <AdrixnDeveloper29@gmail.com>
2026-07-12 03:33:09 -0400

    feat: agrega perfil AutoMapper GetInternalUserDto -> DeveloperListItemViewModel

---
+++ Source/Core/RealEstateApp.Core.Application/Mapping/DtoToViewModelAndReverse/Users/Consult/GetInternalUserDtoToDeveloperViewModel.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using RealEstateApp.Core.Application.DTOs.Users.DtoQueryUser;
+using RealEstateApp.Core.Application.ViewsModel.Users.Consult;
+
+namespace RealEstateApp.Core.Application.Mapping.DtoToViewModelAndReverse.Users.Consult
+{
+    public sealed class GetInternalUserDtoToDeveloperViewModel : Profile
+    {
+        public GetInternalUserDtoToDeveloperViewModel()
+        {
+            CreateMap<GetInternalUserDto, DeveloperListItemViewModel>();
+        }
+    }
+}
