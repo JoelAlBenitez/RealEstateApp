8766c073eaf446ffbadf6ce9947627cc776fe0ce
Joel <278460652+JoelAlBenitez@users.noreply.github.com>
2026-07-11 20:33:59 -0400

    Add interfaces and services for internal user management
    Introduced `IAutheAccountWebApi` and `IOperationalAccountWebApi`
    interfaces to define contracts for authentication and operational
    user management. Added `AuthAccountWebApi` and `OperationalAccountWebApi`
    classes to implement these interfaces, including methods for login,
    role-based user retrieval, and user management operations.
    
    Fixed a typo in an error message in `BaseAccountUser.cs`. Removed
    an unused folder reference from `RealEstateApp.Infraestructure.Identity.csproj`.

---
+++ Source/Core/RealEstateApp.Core.Application/Contracts/Users/InternalUsers/IOperationalAccountWebApi.cs
@@ -0,0 +1,15 @@
+﻿using RealEstateApp.Core.Application.DTOs.Users.DtoQueryUser;
+using RealEstateApp.Core.Application.DTOs.Users.Operational;
+using RealEstateApp.Core.Application.DTOs.Users.Response;
+using RealEstateApp.Core.Domain.Common.Enums;
+
+namespace RealEstateApp.Core.Application.Contracts.Users.InternalUsers
+{
+    public interface IOperationalAccountWebApi
+    {
+        Task<UserResponseDto> CreateInternalUserAsync(RegisterInternalUsersDto register);
+        Task<UserResponseDto> UpdateInternalUserAsync(EditInternalUserDto edit);
+        Task<IReadOnlyCollection<GetInternalUserDto>> GetAllInternalUsersByRol(Roles roles);
+        Task<IReadOnlyCollection<AdminConsultAgentDto>> GetAllAgentesByConsultAdmin();
+    }
+}
---
+++ Source/Infraestructure/RealEstateApp.Infraestructure.Identity/Services/InternalUsers/AuthAccountWebApi.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RealEstateApp.Core.Application.Contracts.Users.InternalUsers;
+using RealEstateApp.Core.Application.DTOs.Users.Auth;
+using RealEstateApp.Core.Application.DTOs.Users.Response;
+using RealEstateApp.Core.Domain.Common.Enums;
+using RealEstateApp.Infraestructure.Identity.Entities;
+
+namespace RealEstateApp.Infraestructure.Identity.Services.InternalUsers
+{
+    public sealed class AuthAccountWebApi : IAutheAccountWebApi
+    {
+        private readonly UserManager<AppUsers> _userManager;
+        private readonly SignInManager<AppUsers> _signInManager;
+
+        public AuthAccountWebApi(
+            UserManager<AppUsers> userManager,
+            SignInManager<AppUsers> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<UserResponseDto> LoginAsync(LoginDto loginDto)
+        {
+            var response = new UserResponseDto
+            {
+                Errors = new List<string>(),
+                HasError = false,
+                Roles = new List<string>()
+            };
+
+
+
+            return response;
+        }
+
+        #region private methods
+
+        private async Task<UserResponseDto> ValidateLogin(UserResponseDto response,
+          AppUsers user, LoginDto dto)
+        {
+            if (string.IsNullOrWhiteSpace(dto.Password) || string.IsNullOrWhiteSpace(dto.EmailOrNameUser))
+            {
+                response.HasError = true;
+                response.Errors.Add("Oops, Los datos de acceso son inválidos.");
+                return response;
+            }
+            if (!user.EmailConfirmed && !user.IsActive)
+            {
+                response.HasError = true;
+                response.Errors.Add("Su cuenta se encuentra inactiva." +
+                    "Pongase en contacto con un administrador.");
+                return response;
+            }
+            var rolesUser = await _userManager.GetRolesAsync(user);
+            if (rolesUser.Contains(Roles.Desarrollador.ToString()))
+            {
+                response.HasError = true;
+                response.Errors.Add("El usuario no tiene un rol válido asignado." +
+                    " Póngase en contacto con un administrador.");
+                return response;
+            }
+
+            var verifyUser = await _signInManager.PasswordSignInAsync(user, dto.Password, false, true);
+            if (!verifyUser.Succeeded)
+            {
+                response.HasError = true;
+                if (verifyUser.IsLockedOut)
+                {
+                    response.Errors.Add("Su cuenta se encuentra bloqueada por multiples intentos repetidos fallidos." +
+                        " Favor intente nuevamente dentro de 15 minutos o contacte con un administrador.");
+                    return response;
+                }
+                else
+                {
+                    response.Errors.Add("El nombre de usuario o la contraseña son incorrectos.");
+                    return response;
+                }
+            }
+
+            return response;
+        }
+
+        #endregion
+    }
+}
