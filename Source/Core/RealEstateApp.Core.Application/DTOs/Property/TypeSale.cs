dc28ddad71dac59a786c439c9e3c4b98860f0195
Adrixn23 <AdrixnDeveloper29@gmail.com>
2026-07-17 20:29:33 -0400

    fix: TypeSale usa required int Id y required string Name para consistencia con TypeProperty

--- Source/Core/RealEstateApp.Core.Application/DTOs/Property/TypeSale.cs
+++ Source/Core/RealEstateApp.Core.Application/DTOs/Property/TypeSale.cs
@@ -2,7 +2,7 @@ namespace RealEstateApp.Core.Application.DTOs.Property
 {
     public sealed class TypeSale
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
+        public required int Id { get; set; }
+        public required string Name { get; set; }
     }
 }
